df081f7dc20528ab8d8082f2996e2eead1321ba2
AdrianAmore <adampe@floridauniversitaria.es>
2021-09-22 19:38:03 +0200

    AE1 Terminada actividad 2

--- AE1/src/es/florida/ae1/App.java
+++ AE1/src/es/florida/ae1/App.java
@@ -1,12 +1,42 @@
 package es.florida.ae1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
 	public static void main(String[] args) {
-		
+
 	}
+
+	// Actividad 1
 	public static void sayHello() {
 		System.out.println("Hola Mundo");
 	}
+
+	// Actividad 2
+	public static void muestraCompas() {
+		// 2-a
+		String[] compasArray = { "Jose", "Ruben", "Adrian", "Fran", "Enrique", "Alejandro" };
+		System.out.println("----Parte 1----");
+		for (int i = 0; i < compasArray.length; i++) {
+			System.out.println(compasArray[i]);
+		}
+
+		// 2-b
+		List<String> compasLista = new ArrayList<String>();
+		compasLista.add("Jose");
+		compasLista.add("Ruben");
+		compasLista.add("Adrian");
+		compasLista.add("Fran");
+		compasLista.add("Enrique");
+		compasLista.add("Alejandro");
+		System.out.println("----Parte 2----");
+		for (int i = 0; i < compasLista.size(); i++) {
+			System.out.println(compasLista.get(i));
+		}
+	}
+	//Actividad 3
+	
 
 }
