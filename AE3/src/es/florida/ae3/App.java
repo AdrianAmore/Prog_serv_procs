ac093868707c3028daae87bcdb54024178e92b62
AdrianAmore <adampe@floridauniversitaria.es>
2021-11-11 18:26:12 +0100

    comentado

--- AE3/src/es/florida/ae3/App.java
+++ AE3/src/es/florida/ae3/App.java
@@ -3,11 +3,11 @@ package es.florida.ae3;
 import java.util.ArrayList;
 
 public class App {
-
+//Aunque la app cuenta con la clase ventilador no he hecho la ampliación
 	public static void main(String[] args) {
 		Ventilador vent = new Ventilador();
 		Mina mine = new Mina(1000);
-		ArrayList<Minero> miners = new ArrayList<Minero>();
+		ArrayList<Minero> miners = new ArrayList<Minero>(); //Array de mineros donde se almacenan todos los mineros que van a trabajar en la mina
 		Thread t;
 		for (int i = 0; i < 10; i++) {
 			Minero miner = new Minero(mine);
@@ -17,13 +17,13 @@ public class App {
 			t.start();
 		}
 		try {
-			Thread.sleep(12000);
+			Thread.sleep(12000); //Tiempo de espera para que no saque el mensaje final antes que los mensajes de extracción
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		int contador = 0;
-		for (Minero p : miners) {
+		for (Minero p : miners) { //Recorremos la array de mineros sumando todo el oro de sus bolsas para ver cuanto oro se ha extraido
 			contador += p.getBolsa();
 		}
 		System.err.println("Total de oro extraido: " + contador);
--- AE3/src/es/florida/ae3/Minero.java
+++ AE3/src/es/florida/ae3/Minero.java
@@ -2,22 +2,30 @@ package es.florida.ae3;
 
 public class Minero implements Runnable {
 
-	int bolsa;
-	int tiempoExtraccion = 1000;
-	Mina mina;
+	int bolsa; //Acumulador del oro extraido
+	int tiempoExtraccion = 1000; //Tiempo en milisegundos para cada turno
+	Mina mina; //La mina sobre la que van a trabajar los mineros
 
 	public Minero(Mina m) {
 		this.bolsa = 0;
 		this.mina = m;
 	}
 
+	/*
+	 * extraerRecurso()
+	 * Recibe: 
+	 * -String(Nombre): el nombre del hilo (minero) que extrae el oro
+	 * -int(oro): la cantidad que se va a extraer por ejecución
+	 * Devuelve: void
+	 */
+	
 	public void extraerRecurso(String nombre, int oro) throws InterruptedException {
 		synchronized (mina) {
 			while (mina.stock > 0) {
-				Thread.sleep(tiempoExtraccion);
+				Thread.sleep(tiempoExtraccion); //Tiempo entre turnos
 				if (oro <= mina.stock) {
-					mina.stock = (mina.stock - oro);
-					this.bolsa = this.bolsa + oro;
+					mina.stock = (mina.stock - oro); //Actualizamos el oro restante en la mina
+					this.bolsa = this.bolsa + oro; //Actualizamos el oro que ha sido extraido
 					System.out.println(nombre + " extrae " + oro + " de oro | " + "Queda: " + mina.stock);
 				}
 			}
